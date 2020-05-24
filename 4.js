892b06ccd8fe064954ecc01142f3c5767115993e
bephia0822 <bephia0822@gmail.com>
2020-05-24 18:45:51 +0900

    Todo app phase 1 +CSS classes

--- 4.js
+++ 4.js
@@ -21,3 +21,32 @@ function App() {
 }
 
 export default App
+
+//這邊他要提醒的是要連到CSS時是className不是Class
+import React from "react"
+
+function Header() {
+    return (
+        <header className="navbar">This is the header</header>
+    )
+}
+
+export default Header
+
+
+body {
+    margin: 0;
+}
+
+//style.css裡面
+.navbar {
+    height: 100px;
+    background-color: #333;
+    color: whitesmoke;
+    margin-bottom: 15px;
+    text-align: center;
+    font-size: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
