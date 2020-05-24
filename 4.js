54f9161838844a63e438ef3e7f8f7d68312f4b7b
bephia0822 <bephia0822@gmail.com>
2020-05-24 16:39:30 +0900

    React Todo app - phase 1

---
+++ 4.js
@@ -0,0 +1,23 @@
+//每一個input type="checkbox"在網頁上都是check list的小框框
+
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+            
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+            
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+            
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+        </div>
+    )
+}
+
+export default App
