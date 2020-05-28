c2908c3b1e83f7c4f505abd3f06e674a74448296
bephia0822 <bephia0822@gmail.com>
2020-05-28 23:27:10 +0900

    Props

--- 7.js
+++ 7.js
@@ -1,1 +1,105 @@
-ggg
+//這是在App.js裡的
+import React from "react"
+import ContactCard from "./ContactCard"
+
+function App() {
+    return (
+        <div className="contacts">
+            <ContactCard 
+                name="Mr. Whiskerson" 
+                imgUrl="http://placekitten.com/300/200" 
+                phone="(212) 555-1234" 
+                email="mr.whiskaz@catnap.meow"
+            />
+            
+            <ContactCard 
+                name="Fluffykins" 
+                imgUrl="http://placekitten.com/400/200" 
+                phone="(212) 555-2345" 
+                email="fluff@me.com"
+            />
+            
+            <ContactCard 
+                name="Destroyer" 
+                imgUrl="http://placekitten.com/400/300" 
+                phone="(212) 555-3456" 
+                email="ofworlds@yahoo.com"
+            />
+            
+            <ContactCard 
+                name="Felix" 
+                imgUrl="http://placekitten.com/200/100" 
+                phone="(212) 555-4567" 
+                email="thecat@hotmail.com"
+            />
+            
+        </div>
+    )
+}
+
+export default App
+
+//這是在ContactCard.js裡的，這是他如何運用props
+import React from "react"
+
+function ContactCard(props) {
+    return (
+        <div className="contact-card">
+            <img src={props.imgUrl}/>
+            <h3>{props.name}</h3>
+            <p>Phone: {props.phone}</p>
+            <p>Email: {props.email}</p>
+        </div>
+    )
+}
+
+export default ContactCard
+
+
+
+//下面是一樣的結果只是寫成這樣如果有很多項時才不會太長
+import React from "react"
+import ContactCard from "./ContactCard"
+
+function App() {
+    return (
+        <div className="contacts">
+            <ContactCard 
+                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
+            />
+            
+            <ContactCard
+                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
+            />
+            
+        </div>
+    )
+}
+
+export default App
+
+
+//ContactCard.js這邊也要改
+import React from "react"
+
+function ContactCard(props) {
+    console.log(props)
+    return (
+        <div className="contact-card">
+            <img src={props.contact.imgUrl}/>
+            <h3>{props.contact.name}</h3>
+            <p>Phone: {props.contact.phone}</p>
+            <p>Email: {props.contact.email}</p>
+        </div>
+    )
+}
+
+export default ContactCard
