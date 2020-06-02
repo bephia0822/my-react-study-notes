2025817093d08bdfc947826b990a39df03a00a9b
bephia0822 <bephia0822@gmail.com>
2020-06-02 22:27:22 +0900

    Todo App : phase3

--- 10.js
+++ 10.js
@@ -1,1 +1,144 @@
-lll
+//做出了真正的to do list!
+//在App.js
+/*
+Let's practice props and mapping components on our todo list app!
+
+I've created a js file with some todos data in it, which I'm imported into this file. (Normally this data would come from an API call, not a local file). 
+
+Challenge: Using the array map method, render a child component for each todo item in the todosData array and pass the relevant data to it.
+*/
+
+import React from "react"
+import TodoItem from "./TodoItem"
+import todosData from "./todosData"
+
+function App() {
+    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+    
+    return (
+        <div className="todo-list">
+            {todoItems}
+        </div>
+    )
+}
+
+export default App
+
+
+//在ToDoItem.js
+import React from "react"
+
+function TodoItem(props) {
+    return (
+        <div className="todo-item">
+            <input type="checkbox" checked={props.item.completed}/>
+            <p>{props.item.text}</p>
+        </div>
+    )
+}
+
+export default TodoItem
+
+
+//在index.html
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="root"></div>
+    <script src="index.pack.js"></script>
+</body>
+</html>
+
+
+
+//在index.js
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+
+//在style.css
+body {
+    background-color: whitesmoke;
+}
+
+.todo-list {
+    background-color: white;
+    margin: auto;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: 1px solid #efefef;
+    box-shadow:
+    /* The top layer shadow */
+        0 1px 1px rgba(0,0,0,0.15),
+            /* The second layer */
+        0 10px 0 -5px #eee,
+            /* The second layer shadow */
+        0 10px 1px -4px rgba(0,0,0,0.15),
+            /* The third layer */
+        0 20px 0 -10px #eee,
+            /* The third layer shadow */
+        0 20px 1px -9px rgba(0,0,0,0.15);
+    padding: 30px;
+}
+
+.todo-item {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 30px 20px 0;
+    width: 70%;
+    border-bottom: 1px solid #cecece;
+    font-family: Roboto, sans-serif;
+    font-weight: 100;
+    font-size: 15px;
+    color: #333333;
+}
+
+input[type=checkbox] {
+    margin-right: 10px;
+    font-size: 30px;
+}
+
+input[type=checkbox]:focus {
+    outline: 0;
+}
+
+
+//在todosData.js
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Clean gecko tank",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Mow lawn",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Catch up on Arrested Development",
+        completed: false
+    }
+]
+
+export default todosData
