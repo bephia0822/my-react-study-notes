7755dae5e2493352a1ebdcdb0fd77b380a8f6a09
bephia0822 <bephia0822@gmail.com>
2020-06-03 00:08:25 +0900

    Class-based components

---
+++ 92.js
@@ -0,0 +1,27 @@
+//他這是教class-based components,在App.js
+import React from "react"
+
+// function App(props) {
+//     return (
+//         <div>
+//             <h1>{props.whatever}</h1>
+//         </div>
+//     )
+// }
+
+class App extends React.Component {
+    
+    yourMethodHere() {
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.props.whatever}</h1>
+            </div>
+        )
+    }
+}
+
+export default App
