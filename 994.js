dfc752a889b3cdc1b7ad70ec8a679fdc2713288b
bephia0822 <bephia0822@gmail.com>
2020-06-11 17:18:51 +0900

    Todo App: phase 7

--- 994.js
+++ 994.js
@@ -1,1 +1,28 @@
-lll
+//to do list打勾的地方會出現像是線劃掉的設計
+
+/**
+ * Challenge: Style the completed todo items differently from the incomplete items.
+ */
+
+import React from "react"
+
+function TodoItem(props) {
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
+    
+    return (
+        <div className="todo-item">
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => props.handleChange(props.item.id)}
+            />
+            <p style={props.item.completed ? completedStyle: null}>{props.item.text}</p>
+        </div>
+    )
+}
+
+export default TodoItem
